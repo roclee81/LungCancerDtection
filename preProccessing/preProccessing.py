203d1435f9aabe295fc77291027d1c0e463166eb
@SiaFahim <git@fahimi.me>
2017-03-08 19:28:05 -0800

    fix the air/ rescale

--- preProccessing/preProccessing.py
+++ preProccessing/preProccessing.py
@@ -29,4 +29,26 @@ def load_scan(path):
 
         return slices
 
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+
+        image[slice_number] += np.int16(intercept)
+
+    return np.array(image, dtype=np.int16)
 
