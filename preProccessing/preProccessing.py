f6866bda3ba1b4a719c8012d2b6ddd1ec7acef4d
@SiaFahim <git@fahimi.me>
2017-03-08 18:29:05 -0800

    preProccessing added

---
+++ preProccessing/preProccessing.py
@@ -0,0 +1,28 @@
+%matplotlib inline
+
+import numpy as np
+import pandas as pd
+import dicom
+import os
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+INPUT_FOLDER = '../Projects/LungCancer/PatientData/sample_images'
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
+
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in oslistdir(path)]
+    slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+        return slices
