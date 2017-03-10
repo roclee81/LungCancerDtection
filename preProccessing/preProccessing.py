389a90a500553eea7c2aa44ff26acd0f8aa9eda8
@SiaFahim <git@fahimi.me>
2017-03-09 19:27:13 -0800

    Showing Sample Image P0

--- preProccessing/preProccessing.py
+++ preProccessing/preProccessing.py
@@ -7,18 +7,24 @@ import dicom
 import os
 import scipy.ndimage
 import matplotlib.pyplot as plt
+import glob
 
 from skimage import measure, morphology
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 INPUT_FOLDER = '/Users/Odyssey/Projects/LungCancer/PatientData/sample_images/'
-patients = os.listdir(INPUT_FOLDER)
+
+def listdir_nohidden(path):
+    return glob.glob(os.path.join(path, '*'))
+patients = listdir_nohidden(INPUT_FOLDER)
 patients.sort()
 
 #load scans in given folder path to INPUT_FOLDER
+
 def load_scan(path):
     slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
     slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+
     try:
         slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
     except:
@@ -28,7 +34,7 @@ def load_scan(path):
         s.SliceThickness = slice_thickness
 
         return slices
-
+INPUT_FOLDER
 def get_pixels_hu(slices):
     image = np.stack([s.pixel_array for s in slices])
     # Convert to int16 (from sometimes int16)
@@ -53,7 +59,7 @@ def get_pixels_hu(slices):
     return np.array(image, dtype=np.int16)
 
 #view one of the patients
-first_patient = load_scan(+INPUT_FOLDER  patients[0])
+first_patient = load_scan(patients[0])
 first_patient_pixels = get_pixels_hu(first_patient)
 plt.hist(first_patient_pixels.flatten(), bins=80, color='C')
 plt.xlabel("Houfnfield Units (HU)")
@@ -63,5 +69,7 @@ plt.show()
 #show an slice in the middle
 plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
 plt.show()
+
+
 
 
