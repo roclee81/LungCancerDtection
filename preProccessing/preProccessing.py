9bb92e700d55e45f7e099122f75e848e37616dce
@SiaFahim <git@fahimi.me>
2017-04-04 22:21:03 -0700

    Segmentation

--- preProccessing/preProccessing.py
+++ preProccessing/preProccessing.py
@@ -34,7 +34,7 @@ def load_scan(path):
         s.SliceThickness = slice_thickness
 
         return slices
-INPUT_FOLDER
+
 def get_pixels_hu(slices):
     image = np.stack([s.pixel_array for s in slices])
     # Convert to int16 (from sometimes int16)
@@ -61,15 +61,113 @@ def get_pixels_hu(slices):
 #view one of the patients
 first_patient = load_scan(patients[0])
 first_patient_pixels = get_pixels_hu(first_patient)
-plt.hist(first_patient_pixels.flatten(), bins=80, color='C')
-plt.xlabel("Houfnfield Units (HU)")
-plt.ylabel("Frequency")
-plt.show()
+#plt.hist(first_patient_pixels.flatten(), bins=80, color='C')
+#plt.xlabel("Houfnfield Units (HU)")
+#plt.ylabel("Frequency")
+#plt.show()
 
-#show an slice in the middle
-plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
-plt.show()
+# Show some slice in the middle
+#plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
+#plt.show()
+
+def resample(image, scan, new_spacing=[1,1,1]):
+    # Determine current pixel spacing
+    spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
+
+    resize_factor = spacing / new_spacing
+    new_real_shape = image.shape * resize_factor
+    new_shape = np.round(new_real_shape)
+    real_resize_factor = new_shape / image.shape
+    new_spacing = spacing / real_resize_factor
+
+    image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
+
+    return image, new_spacing
 
 
+# Please note that when you apply this, to save the new spacing! Due to rounding this may be slightly off from the desired spacing (above script picks the best possible spacing with rounding).
+# Let's resample our patient's pixels to an isomorphic resolution of 1 by 1 by 1 mm.
+
+pix_resampled, spacing = resample(first_patient_pixels, first_patient, [1,1,1])
+#print("shape before resampling\t", first_patient_pixels.shape)
+#print("Shape after resampling\t", pix_resampled.shape)
 
 
+def plot_3d(image, threshold=-300):
+    # Position the scan upright,
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2, 1, 0)
+
+    verts, faces = measure.marching_cubes(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+
+#plot_3d(pix_resampled, 400)
+
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)
+
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]
+
+    if len(counts) > 0:
+        return vals[np.argmax(counts)]
+    else:
+        return None
+
+
+def segment_lung_mask(image, fill_lung_structures=True):
+    # not actually binary, but 1 and 2.
+    # 0 is treated as background, which we do not want
+    binary_image = np.array(image > -320, dtype=np.int8) + 1
+    labels = measure.label(binary_image)
+
+    # Pick the pixel in the very corner to determine which label is air.
+    #   Improvement: Pick multiple background labels from around the patient
+    #   More resistant to "trays" on which the patient lays cutting the air
+    #   around the person in half
+    background_label = labels[0, 0, 0]
+
+    # Fill the air around the person
+    binary_image[background_label == labels] = 2
+
+    # Method of filling the lung structures (that is superior to something like
+    # morphological closing)
+    if fill_lung_structures:
+        # For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
+
+            if l_max is not None:  # This slice contains some lung
+                binary_image[i][labeling != l_max] = 1
+
+    binary_image -= 1  # Make the image actual binary
+    binary_image = 1 - binary_image  # Invert it, lungs are now 1
+
+    # Remove other air pockets insided body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+    if l_max is not None:  # There are air pockets
+        binary_image[labels != l_max] = 0
+
+    return binary_image
+
+segmented_lungs = segment_lung_mask(pix_resampled, False)
+segmented_lungs_fill = segment_lung_mask(pix_resampled, True)
+
+plot_3d(segmented_lungs, 0)
